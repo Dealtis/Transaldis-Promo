f7bf6442f996852e0388310441f89890683d7ba6
eXt-Ra <vanardois.romain@gmail.com>
2016-05-18 11:11:43 +0200

    initial commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name transaldisSitePromoApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the transaldisSitePromoApp
+ */
+angular.module('transaldisSitePromoApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name transaldisSitePromoApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the transaldisSitePromoApp
+ */
+angular.module('transaldisSitePromoApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
